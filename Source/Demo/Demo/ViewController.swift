bf9ba55bb759eac8f84627142c9f0f62795bcb55
xiaoyuan <xiaoyuan@xunlei.com>
2017-06-17 22:03:41 +0800

    fix

--- Source/Demo/Demo/ViewController.swift
+++ Source/Demo/Demo/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func click() {
-        var alertView = AlertView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        let alertView = AlertView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
         alertView.backgroundColor = UIColor.red
         alertView.show(in: view)
     }
